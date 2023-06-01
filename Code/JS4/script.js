c9db7988f9cdccaeb75bdd7a358f62a1c7487394
ramymibrahim <ramymibrahim@yahoo.com>
2023-06-01 15:09:46 +0300

    Update script.js

--- Code/JS4/script.js
+++ Code/JS4/script.js
@@ -1,19 +1,45 @@
-// String functions
-//replace,trim,startsWith,endsWith,split,join,indexOf (-2),lastIndexOf
+/*
+Edit function
+Add Task datetime, task status, Assigned to
+// Replace,trim,startsWith,endsWith,split,join,indexOf (-2),lastIndexOf
 
-// Destructing Arrays and Objects
+Enter test "IMP:Task1, Task2(3), Task3(4)", If it starts with IMP: make it priority 1
+Enter task in it's position in the array
+Generate one line of code with task names separated by comma
 // Rest (signature) and Spread Operators (pass)
-// Arrays again (new Array)
+// Destructing Arrays and Objects
+// Reference Type and Value type copy
+Copy task for today button
+
 // indexOf, includes, sort
+Sort tasks by column
+
 // Array functions (filter, find, map, reduce, forEach,every , some,flatMap)
+Filter tasks by name
+Get tasks count for today
+Check if some tasks are important (make it with other color)
+
 // Maps and Sets
+Get tasks count for each day
+Talk about the HashSet and Dictionary
+
+Add user as normal object
+Change task status (Encapsulation Problem)
+Balance Problem
 // OOP
-// APIs
-// Async Await
-// Micro and Macro task queues
-// Call stack
+implement mark as done/undone using OOP
+Add tasks with subtasks
 
 // DOM and BOM
 // Query Selector, byId, Tag, ClassName
-//IIFE
-//Modules
+Add checkbox for delete many
+
+
+// APIs
+// Async Await
+// Micro and Macro task queues & Call stack
+
+// IIFE
+// Modules
+// JQuery Intro
+*/
